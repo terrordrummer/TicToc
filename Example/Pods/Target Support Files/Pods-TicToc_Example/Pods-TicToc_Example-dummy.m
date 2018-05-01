6c186a119fcacabc42193ecde0a6322e0cc68ba6
Roberto Sartori <roberto.sartori@gmail.com>
2018-05-01 14:12:35 +0200

    Initial setup

---
+++ Example/Pods/Target Support Files/Pods-TicToc_Example/Pods-TicToc_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TicToc_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TicToc_Example
+@end
---
+++ Example/Pods/Target Support Files/TicToc/TicToc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TicToc : NSObject
+@end
+@implementation PodsDummy_TicToc
+@end
